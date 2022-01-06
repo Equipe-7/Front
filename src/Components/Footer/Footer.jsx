b03e0778026a3e6e99c46cfa9987f1f1bac5b301
yago.monteiroo@gmail.com <yago.monteiroo@gmail.com>
2022-01-06 17:16:59 -0300

    footer development
    footer development

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,68 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import Logo from "../../Assets/img/sevenpets-h2.png";
 
 const Footer = () => {
-    return (
-        <footer>
-            <h1>footer</h1>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <section className="footer-container">
+        <div className="logo">
+          <img className="logo-img" src={Logo} alt="" />
+          <h2 className="logo-slogan">Loucos por pets, assim como você!</h2>
+        </div>
 
-export default Footer
+        <div className="footer-itens">
+          <ul className="itens-container">
+            <h3 className="item-title"> Titulo</h3>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+          </ul>
+
+          <ul className="itens-container">
+            <h3 className="item-title"> Titulo</h3>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+          </ul>
+
+          <ul className="itens-container">
+            <h3 className="item-title"> Titulo</h3>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+            <li className="item">
+              <a>footer item</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
